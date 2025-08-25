6c01b3216809b084865dbc413aed60304f79e548
João Victor <victorvlone999@gmail.com>
2025-08-25 09:42:40 -0300

    feat: creat class ChessPeace

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,23 @@
+package chess;
+
+import boardGame.Board;
+import boardGame.Piece;
+
+public class ChessPiece extends Piece {
+
+    private Color color;
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+}
