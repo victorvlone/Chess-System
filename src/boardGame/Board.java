a6cd516a08690ac513b7a067429821d546d57d49
João Victor <victorvlone999@gmail.com>
2025-08-25 09:38:54 -0300

    feat: create class Color

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -12,6 +12,31 @@ public class Board {
         pieces = new Piece[rows][columns];
     }
 
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+    public Integer getColumns() {
+        return columns;
+    }
+
+    public void setColumns(Integer columns) {
+        this.columns = columns;
+    }
+
+    public Piece piece(Integer row, Integer column){
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
+    
     
 
 }
