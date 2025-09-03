a646944a218c715f533734d80f5cc8717ab2e079
João Victor <victorvlone999@gmail.com>
2025-09-03 08:14:35 -0300

    feat: create class Pawn

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,7 +7,7 @@ import boardGame.Position;
 public abstract class ChessPiece extends Piece {
 
     private Color color;
-    private Integer moveCount;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
